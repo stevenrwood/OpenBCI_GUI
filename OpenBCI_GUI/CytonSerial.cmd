2ddf6e52c2fdd74c8691c959f75a0a6bc0985598
Steve Wood <stevenrwood@live.com>
2021-02-04 10:35:50 -0800

    Make test scripts work at home and at work.
    Don't limit analog read channels if WiFi protocol.  Always show 3 channels.

--- OpenBCI_GUI/CytonSerial.cmd
+++ OpenBCI_GUI/CytonSerial.cmd
@@ -1,2 +1,8 @@
-%PROCESSING%\processing-java.exe --sketch=%USERPROFILE%\github\OpenBCI_GUI\OpenBCI_GUI --run --cyton --daisy --auxinput "\users\steve\bciGame\BciGame.exe --game FollowMe --logFolder $session --openBCIPort $bciport --StimulusDelay 1000 --FeedbackDelay 500 --TrialCount 500 --openBCIPort $bciport"
+@if "%_echo%" == "" echo off
+if EXISTS \\fileserver\user\stevew\BCIGame\v1.5\bcigame.exe (
+    set bcigamePath=\\fileserver\user\stevew\BCIGame\v1.5
+) else (
+    set bcigamePath=%USERPROFILE%\bcigame
+)
+%PROCESSING%\processing-java.exe --sketch=%USERPROFILE%\github\OpenBCI_GUI\OpenBCI_GUI --run --cyton --daisy --auxinput "%bcigamePath%\BciGame.exe --game FollowMe --logFolder $session --openBCIPort $bciport --StimulusDelay 1000 --FeedbackDelay 500 --TrialCount 500 --openBCIPort $bciport"
 
